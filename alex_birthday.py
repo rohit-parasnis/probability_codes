a4be9b4e49ee154e360f0428c2b92efa188ef957
rohit-parasnis <31666230+rohit-parasnis@users.noreply.github.com>
2017-09-05 11:35:46 -0700

    Update alex_birthday.py

--- alex_birthday.py
+++ alex_birthday.py
@@ -1,15 +1,12 @@
-import itertools
-import random
-
 import matplotlib.pyplot as plt
 import numpy as np
 
-from ipywidgets import interact, interactive, fixed, interact_manual
+# from ipywidgets import interact, interactive, fixed, interact_manual
 import ipywidgets as widgets
 
 
 # Range of number of people
-PEOPLE = np.arange(1, 26)
+num_PEOPLE = np.arange(1, 26)
 
 # Days in year
 DAYS = 365
@@ -42,16 +39,16 @@ def plot_probs(iterations):
     prob = []         # Theoretical prob. of unique-birthday sample
     
     # Compute data points to plot
-    np.random.seed(1)
-    for num_people in PEOPLE:
+    # np.random.seed(1)
+    for num_people in num_PEOPLE:
         unique_count = sum(sample_unique_birthdays(num_people)
                           for i in range(iterations))
         sample_prob.append(unique_count / iterations)
         prob.append(prob_unique_birthdays(num_people))
     
     # Plot results
-    plt.plot(PEOPLE, prob, 'k-', label='Theoretical prob.')
-    plt.plot(PEOPLE, sample_prob, 'bo-', label='Empirical prob.')
+    plt.plot(num_PEOPLE, prob, 'k-', label='Theoretical prob.')
+    plt.plot(num_PEOPLE, sample_prob, 'bo-', label='Empirical prob.')
     plt.axhline(0.5, color='red', label='0.5 threshold')
     plt.xlabel('Number of people')
     plt.ylabel('Probability of unique birthdays')
@@ -61,6 +58,6 @@ def plot_probs(iterations):
     plt.show()
 
     
-interact(plot_probs,
+widgets.interact(plot_probs,
          iterations=widgets.IntSlider(min=50, max=5050, step=200),
          continuous_update=False, layout='bottom');
